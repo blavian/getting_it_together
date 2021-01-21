133d995fab692273b1af85e4fb9d2a10803fbd5d
Benjamin Lavian <benylavian@gmail.com>
2021-01-20 21:00:29 -0500

    backend test route working

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,19 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-const { environment } = require("./config");
+
 const routes = require("./routes");
+const { environment } = require("./config");
 const isProduction = environment === "production";
+
 const app = express();
 
 app.use(morgan("dev"));
+
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
+
+
 
 // Security Middleware
 if (!isProduction) {
@@ -34,4 +38,5 @@ app.use(
     },
   })
 );
+app.use(routes);
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+
+
 router.get("/hello/world", function (req, res) {
   res.cookie("XSRF-TOKEN", req.csrfToken());
   res.send("Hello World!");
