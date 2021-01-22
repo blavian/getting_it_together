963570b3350fae948a228dee7d386dd937b57e2c
Benjamin Lavian <benylavian@gmail.com>
2021-01-22 13:28:36 -0500

    signup working

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,6 +33,19 @@ export const restoreUser = () => async (dispatch) => {
   dispatch(setUser(res.data.user));
   return res;
 };
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const res = await fetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  dispatch(setUser(res.data.user));
+  return res;
+};
 
 
 const initialState = { user: null };
