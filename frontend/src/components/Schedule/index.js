265545424faa77e9f556673c0f3ad41898366fb1
Benjamin Lavian <benylavian@gmail.com>
2021-01-26 15:56:04 -0500

    using react for frontend

--- frontend/src/components/Schedule/index.js
+++ frontend/src/components/Schedule/index.js
@@ -1,5 +1,24 @@
+import{useEffect, useState} from 'react'
+import {fetch} from '../../store/csrf'
+
 const Schedule = ()=>{
-    return <h1>just checking</h1>
+  const[currentSchedule,setSchedule] = useState([])
+  
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   useEffect(async() => {
+       const response = await fetch('/api/schedule')
+       setSchedule(response.data.schedule)
+   }, []);
+    return(
+        <div className="container">
+        <h1> Children</h1>
+        {!currentSchedule && <h1> Add child</h1>}
+        {currentSchedule && currentSchedule.map(child=>{
+          return <h1>{child.name}</h1>
+        })}
+        </div>
+
+    )
 }
 
 export default Schedule
