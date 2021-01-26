117d320cc2a901365b7cf99fa215929097c91044
Benjamin Lavian <benylavian@gmail.com>
2021-01-25 21:13:48 -0500

    somewhat working hamburger menu not working

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
@@ -6,21 +6,10 @@ import LoginFormModal from "../LoginFormModal";
 import "./Navigation.css";
 import styled from "styled-components"
 
-const Navbar = styled.div`
-display:flex;
-justify-content:space-between;
-align-items:center;
-flex-wrap:wrap;
-background:yellow;
-`
-const hamburger = styled.div``
-
-
-
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
-
+const[open,setOpen] = useState('false')
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = <ProfileButton user={sessionUser} />;
@@ -28,7 +17,7 @@ function Navigation({ isLoaded }) {
     sessionLinks = (
       <>
         <LoginFormModal />
-        <NavLink to="/signup">Sign Up</NavLink>
+        <MenuLink href="/signup">Sign Up</MenuLink>
       </>
     );
   }
@@ -38,7 +27,7 @@ function Navigation({ isLoaded }) {
     <Logo href="">
     GettingIt<span>2gether</span>
     </Logo>
-    <Hamburger>
+    <Hamburger onClick={()=>setOpen(!open)}>
       <span />
       <span />
       <span />
@@ -53,11 +42,55 @@ function Navigation({ isLoaded }) {
       </Nav>
   );
 }
-const Nav = styled.div``;
-const Hamburger = styled.div``;
-const MenuLink = styled.div``;
-const Menu = styled.div``;
-const Logo = styled.div``;
+const Nav = styled.div`
+padding: 0 2 rem;
+display: flex:
+justify-content: space-between;
+align-items: center;
+flex-wrap: wrap;
+background: white;
+`;
+
+const Hamburger = styled.div`
+display:none;
+flex-direction:column;
+cursor:pointer;
+span{
+  height:2px;
+  width:25px;
+  background:#7b7fda
+  margin-bottom:4px;
+  border-radius:5px;
+}
+@media(max-width:760px){
+  display:flex;
+}
+`
+const MenuLink = styled.a`
+padding: 1rem 2rem;
+cursor:pointer;
+text-align:center;
+text-decoration:none;
+color:#67bc98;
+transition: all 0.3s ease-in;
+font-size:0.9 rem;
+&:hover{
+  color:#7b7fda
+}
+`;
+const Menu = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+position:relative;
+
+`;
+const Logo = styled.div`
+padding:1 rem 0;
+color:red;
+text-decoration:none;
+font-weight:800
+`;
 
 
 
