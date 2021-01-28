1cb445f0755b15275ba5ac1842f62961233e8fe9
Benjamin Lavian <benylavian@gmail.com>
2021-01-28 15:44:50 -0500

    children component now redirects

--- frontend/src/components/Schedule/index.js
+++ frontend/src/components/Schedule/index.js
@@ -2,6 +2,7 @@ import{useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { fetchAllChildren } from "../../store/children";
 import './schedule.css'
+import {Link} from 'react-router-dom'
 
 const Schedule = ()=>{
   const dispatch = useDispatch()
@@ -24,14 +25,18 @@ const Schedule = ()=>{
         {!currentChildren && <h1> Add child</h1>}
         {currentChildren &&
           currentChildren.map((child) => {
-            return(
-              <div className="card">
-              <div class="name">
-             <h1>{child.first_name} {child.last_name}</h1>
-             <h2>Age:{child.age}</h2>
-              </div>
-              </div>
-            )
+            return (
+              <>
+                <Link to="/child/:childid">
+                  <div className="card">
+                    <div class="name">
+                      <h1>{child.first_name}</h1>
+                      <h2>Age:{child.age}</h2>
+                    </div>
+                  </div>
+                </Link>
+              </>
+            );
           })}
       </div>
     );
