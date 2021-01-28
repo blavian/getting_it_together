41bb360edaa1f1c6da9528dba5e3c632ef079c7a
Benjamin Lavian <benylavian@gmail.com>
2021-01-27 21:23:48 -0500

    foxed where demo user was not showing up

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -31,7 +31,7 @@ function SignupFormPage() {
   };
 
   return (
-    <form className='sign-up' onSubmit={handleSubmit}>
+    <form  onSubmit={handleSubmit}>
       <ul>
         {errors.map((error, idx) => (
           <li key={idx}>{error}</li>
