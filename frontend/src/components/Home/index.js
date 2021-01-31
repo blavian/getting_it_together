de3679f7ed1d3777704418e2e1d509555a371990
Benjamin Lavian <benylavian@gmail.com>
2021-01-30 20:31:43 -0500

    basic homepage

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -3,26 +3,26 @@ import homePic from "./rachel-o3tIY5pIork-unsplash.jpg";
 import {useSelector} from 'react-redux'
 import Schedule from "../Schedule"
 
-const Home = ()=>{
-    const sessionUser = useSelector((state) => state.session.user);
-    if(sessionUser){
-
-        return (
-          <div className="container">
-           < Schedule />
-          </div>
-        );
-    } else{
-        return (
-          <div className="container">
-            <h1>
-              Welcome to Getting it together! If you are struggling with
-              managing your kids virtual schedules,this is the place for you.
-            </h1>
-          </div>
-        ); 
-    }
-    }
+const Home = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+  if (sessionUser) {
+    return (
+      <div className="container">
+        <Schedule />
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <img src={homePic} alt="pic" />
+        <h1 className="text">
+          Welcome to Getting it together! If you are struggling with managing
+          your kids virtual schedules,this is the place for you.
+        </h1>
+      </div>
+    );
+  }
+};
 
 
 
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -27,6 +27,7 @@ function ProfileButton({ user }) {
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
+    
   };
 
   return (
